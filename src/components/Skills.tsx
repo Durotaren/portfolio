dbc08d057bc29b5d12d4f614daf1a0072132901c
Durotaren <188862641+Durotaren@users.noreply.github.com>
2026-03-25 21:25:43 +0200

    Develop the CssIcon Component

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,12 +1,13 @@
 import HtmlIcon from './icons/HtmlIcon';
 import TypeScriptIcon from './icons/TypeScript';
+import CssIcon from './icons/CssIcon';
 
 export default function Skills() {
   return (
     <div className="w-full h-[156px] my-6 grid grid-cols-4 grid-rows-2 gap-2">
       <HtmlIcon />
       <TypeScriptIcon />
-      <div className="w-full h-full bg-red-500">3</div>
+      <CssIcon />
       <div className="w-full h-full bg-red-500">4</div>
       <div className="w-full h-full bg-red-500">5</div>
       <div className="w-full h-full bg-red-500">6</div>
