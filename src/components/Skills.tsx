1d29ca8053963108503596c4503aaeea327b2cad
Durotaren <188862641+Durotaren@users.noreply.github.com>
2026-03-25 21:13:04 +0200

    Develop the TypeScript component

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,10 +1,11 @@
 import HtmlIcon from './icons/HtmlIcon';
+import TypeScriptIcon from './icons/TypeScript';
 
 export default function Skills() {
   return (
     <div className="w-full h-[156px] my-6 grid grid-cols-4 grid-rows-2 gap-2">
       <HtmlIcon />
-      <div className="w-full h-full bg-red-500">2</div>
+      <TypeScriptIcon />
       <div className="w-full h-full bg-red-500">3</div>
       <div className="w-full h-full bg-red-500">4</div>
       <div className="w-full h-full bg-red-500">5</div>
