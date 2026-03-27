e705bedbf4e0f82f976013c6e76efb94403a21af
Durotaren <188862641+Durotaren@users.noreply.github.com>
2026-03-27 18:55:49 +0200

    Update the Interactivity component

--- src/components/Interactivity.tsx
+++ src/components/Interactivity.tsx
@@ -10,17 +10,28 @@ export default function Interactivity({
   setIsDragging,
 }: InteractivityProps) {
   return (
-    <div
-      ref={sliderRef}
-      className="w-full h-6 border-white rounded-full bg-white relative mt-2"
-    >
+    <div className="mt-6 flex flex-col gap-4 border-gray-600 border-1 p-4 rounded">
+      <div className="flex flex-col gap-1">
+        <p className="text-gray-400">
+          I like to make things engaging and interactive.
+        </p>
+        <p className="text-gray-400">
+          Here for example you can control time! Don't trust me? Just drag the
+          circle and see for yourself!
+        </p>
+      </div>
       <div
-        className="w-6 h-6 bg-orange-500 absolute rounded-full top-1/2 -translate-y-1/2 -translate-x-1/2"
-        style={{ left: `${position}%` }}
-        onMouseDown={() => {
-          setIsDragging(true);
-        }}
-      ></div>
+        ref={sliderRef}
+        className="w-full h-6 border-white rounded-full bg-gray-500 relative mt-2"
+      >
+        <div
+          className="w-6 h-6 bg-white absolute rounded-full top-1/2 -translate-y-1/2 -translate-x-1/2 hover:cursor-pointer"
+          style={{ left: `${position}%` }}
+          onMouseDown={() => {
+            setIsDragging(true);
+          }}
+        ></div>
+      </div>
     </div>
   );
 }
